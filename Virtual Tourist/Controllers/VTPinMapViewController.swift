5f5b51924d33494c785519bc4133da124f636434
Daniel Pratt <daniel@blaumagier.com>
2017-04-14 12:10:11 -0700

    feat: storyboard view added
    - Added Navigation Controller
    - Added UIViewController with an MKMapView
    - Created VTPinMapViewController to control the root view controller
    - Integrated MapKit and set VTPinMapViewController as an MKMapViewDelegate

---
+++ Virtual Tourist/Controllers/VTPinMapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  VTPinMapViewController.swift
+//  Virtual Tourist
+//
+//  Created by Daniel Pratt on 4/14/17.
+//  Copyright © 2017 Daniel Pratt. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class VTPinMapViewController: UIViewController, MKMapViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
