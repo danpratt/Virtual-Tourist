208010137e5555648f237c3a66f66c56b8470e59
Daniel Pratt <daniel@blaumagier.com>
2017-04-14 13:46:00 -0700

    feat: enabled pin drops
    - setup annotation view
    - enabled gesture recognizer on longpress in storyboard
    - set longpress action to add a pin whenever the user long presses

--- Virtual Tourist/Controllers/VTPinMapViewController.swift
+++ Virtual Tourist/Controllers/VTPinMapViewController.swift
@@ -9,12 +9,72 @@
 import UIKit
 import MapKit
 
+// MARK: - VTPinMapViewController Class
 class VTPinMapViewController: UIViewController, MKMapViewDelegate {
 
+    // MARK: - Class Constants
+    let minPressDuration = 0.75
+    var albumAnnotations = [MKPointAnnotation]()
+    
+    // MARK: - IBOutlets
+    @IBOutlet var longPressRecognizer: UILongPressGestureRecognizer!
+    @IBOutlet weak var pinMapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Setup long press recognizer
+        longPressRecognizer.minimumPressDuration = minPressDuration
+        longPressRecognizer.delaysTouchesBegan = true
+        pinMapView.addGestureRecognizer(longPressRecognizer)
+        pinMapView.delegate = self
+        
     }
+    
 
+    // MARK: - IBActions
+    
+    @IBAction func longPressActionDetected(_ sender: AnyObject) {
+        
+        if longPressRecognizer.state == .began {
+            let position = longPressRecognizer.location(in: pinMapView)
+            print("Position is: \(position)")
+            
+            let coordinate = pinMapView.convert(position, toCoordinateFrom: pinMapView)
+            print("Coordinate is: \(coordinate)")
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            albumAnnotations.append(annotation)
+            pinMapView.addAnnotation(annotation)
+        }
+        
+        
+    }
+    
+    // MARK: - Delegate Methods
+    
+    
+    // Pin view for displaying all pins
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let identifier = "albumLocation"
+        var albumAnnotationsView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+        
+        if albumAnnotationsView == nil {
+            albumAnnotationsView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            albumAnnotationsView?.isDraggable = true
+            albumAnnotationsView?.animatesDrop = true
+            
+        } else {
+            albumAnnotationsView!.annotation = annotation
+        }
+        
+        return albumAnnotationsView
+    }
+    
+    // Updates the MapView when an annotation is added
+    func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+        print("added")
+    }
+    
 }
